4b163cc8420e80b6c1afb8542288e1ba96f73a92
Guitti <guilhermederetti9@gmail.com>
2022-12-15 21:48:21 +0000

    added js and organized the file folders

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var searchParameter = "elections";
+var numberOfRecords = 1;
+var beginDate;
+var endDate;
+
+var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=vSWdiUeg8PPYtjMnjp4rcMkOuBu5Rfny&q=' + searchParameter;
+
+// if (numberOfRecords) {
+//   queryURL += '&page=' + numberOfRecords
+// }
+if (beginDate) {
+  queryURL += '&begin_date=' + beginDate
+}
+if (endDate) {
+  queryURL += '&end_date=' + endDate
+}
+
+console.log(queryURL);
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  .then(function (response) {
+    var results = response.response.docs;
+    console.log(results);
+    for(i = 0; i < numberOfRecords;i++){
+      var snippet = results[i].snippet;
+      var webUrl = results[i].web_url;
+      var pubDate = results[i].pub_date;
+      var headline = results[i].headline.main;
+      var source = results[i].source;
+    }
+  });
